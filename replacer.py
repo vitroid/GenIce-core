69342c1ce2ca3055a1202b697784823b72f107d2
vitroid <vitroid@gmail.com>
2025-06-11 11:40:49 +0900

    -

--- replacer.py
+++
@@ -1,7 +0,0 @@
-import jinja2 as jj
-import toml
-import sys
-
-proj = toml.load("pyproject.toml")
-t = jj.Environment(loader=jj.FileSystemLoader(".")).from_string(sys.stdin.read())
-print(t.render(**proj))
